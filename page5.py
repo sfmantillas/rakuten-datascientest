d3e17de8961f61bed3b9c1288a07390b4e500427
Sebastián Felipe Mantilla Serrano <sfmantillas@gmail.com>
2024-08-06 16:35:02 +0200

    DNN slice formatted

--- page5.py
+++ page5.py
@@ -3,11 +3,6 @@ import pandas as pd
 from PIL import Image
 
 #"🎈 My new app"
-Img_cover = Image.open("Images/0_FrontCover/FotoJet.jpg")
-img_1_Classif_NBC_confmat = Image.open("Images/4_Machile_Learning/1_Classif_NBC_confmat.png")
-img_1_Classif_NBC_report = Image.open("Images/4_Machile_Learning/1_Classif_NBC_report.png")
-
-img_model = Image.open("Images/5_DNN_Model/model.png")
 
 def app():
     
@@ -21,35 +16,36 @@ def app():
     with st.container():
         st.write("")
         
-        st.write(
+        st.markdown(
                 """
+                <p style="font-size: 20px;">
             This model integrates NLP for text and CNNs for image processing. Text data is vectorized using TF-IDF, while images undergo augmentation using Keras' ImageDataGenerator. Key steps include rescaling, shearing, and zooming to enhance robustness and prevent overfitting.
             Model Architecture:
-             - Image Branch: Uses MobileNetV2 for feature extraction, followed by GlobalAveragePooling2D, a dense layer with 512 neurons, and a dropout layer.
-             - Text Branch: Processes TF-IDF vectors with a dense layer of 512 neurons and a dropout layer.
+             - <p style="font-size: 20px;"> Image Branch: Uses MobileNetV2 for feature extraction, followed by GlobalAveragePooling2D, a dense layer with 512 neurons, and a dropout layer.
+             - <p style="font-size: 20px;"> Text Branch: Processes TF-IDF vectors with a dense layer of 512 neurons and a dropout layer.
             """
             ,
             unsafe_allow_html=True
             )
 
-        img_Layers_Sajjad_Neu = Image.open("Images/5_DNN_Model/Layers_Sajjad_Neu.png")
+        img_Layers_Sajjad_Neu = Image.open("Images/5_DNN_Model/Layers_Sajjad_Neu.jpg")
         st.image(img_Layers_Sajjad_Neu)
 
-        st.write(
+        st.markdown(
                 """
-           Both branches are concatenated, and a final dense layer with softmax activation classifies into 27 categories.
+           <p style="font-size: 20px;"> Both branches are concatenated, and a final dense layer with softmax activation classifies into 27 categories.
 
-           Training Setup:
-            - Compile: Optimizer, sparse categorical cross-entropy loss, and accuracy metric.
-            - Data Generators: Batches of 32 for training and validation, with augmentation.
-            - Callbacks: EarlyStopping and ReduceLROnPlateau for monitoring and adjusting training.
+           <p style="font-size: 20px;"> Training Setup:
+            - <p style="font-size: 20px;"> Compile: Optimizer, sparse categorical cross-entropy loss, and accuracy metric.
+            - <p style="font-size: 20px;"> Data Generators: Batches of 32 for training and validation, with augmentation.
+            - <p style="font-size: 20px;"> Callbacks: EarlyStopping and ReduceLROnPlateau for monitoring and adjusting training.
 
-            Results: Achieved performance with 78% accuracy after 20 epochs, with similar training and validation accuracy, indicating no overfitting or underfitting.
+            <p style="font-size: 20px;"> Results: Achieved performance over 75% accuracy after 20 epochs, with similar training and validation accuracy, indicating no overfitting or underfitting.
             """
             ,
             unsafe_allow_html=True
             )
-        img_Performance_Sajjad = Image.open("Images/5_DNN_Model/Performance_Sajjad.png")
+        img_Performance_Sajjad = Image.open("Images/5_DNN_Model/Performance_Sajjad.jpg")
         st.image(img_Performance_Sajjad)
 
     with st.container():
@@ -59,36 +55,36 @@ def app():
         
         st.write(
                 """
-            Overview:
-             - Combines: Text and image data using TensorFlow and Keras.
-             - Text Preprocessing: Tokenization, stop word removal, and TF-IDF vectorization (up to 10,000 features).
-             - Text Model: Dense layers with 128 and 64 neurons, ReLU activation.
-             - Image Model: Input layer (128x128 RGB), three Conv2D layers (32, 64, 128 filters) with ReLU, MaxPooling, and Flatten.
+            <p style="font-size: 20px;">  Overview:
+             - <p style="font-size: 20px;"> Combines: Text and image data using TensorFlow and Keras.
+             - <p style="font-size: 20px;"> Text Preprocessing: Tokenization, stop word removal, and TF-IDF vectorization (up to 10,000 features).
+             - <p style="font-size: 20px;"> Text Model: Dense layers with 128 and 64 neurons, ReLU activation.
+             - <p style="font-size: 20px;"> Image Model: Input layer (128x128 RGB), three Conv2D layers (32, 64, 128 filters) with ReLU, MaxPooling, and Flatten.
 
-            Integration:
-             - Combined Branch: Concatenates text and image features, processes with dense layers (128 and 64 neurons).
-             - Output Layer: Softmax activation for classification into 27 categories.
+            <p style="font-size: 20px;"> Integration:
+             - <p style="font-size: 20px;"> Combined Branch: Concatenates text and image features, processes with dense layers (128 and 64 neurons).
+             - <p style="font-size: 20px;"> Output Layer: Softmax activation for classification into 27 categories.
             """
             ,
             unsafe_allow_html=True
             )
 
-        img_Pry_layer_neu = Image.open("Images/5_DNN_Model/Pry_layer_neu.png")
+        img_Pry_layer_neu = Image.open("Images/5_DNN_Model/Pry_layer_neu.jpg")
         st.image(img_Pry_layer_neu)
 
         st.write(
                 """
-           Training:
-            - Data Generators: Augmented text and image data (batch size: 32).
-            - Compile: Adam optimizer, categorical cross-entropy loss, accuracy metric.
-            - Train/Validation Split: 80/20.
+           <p style="font-size: 20px;"> Training:
+            - <p style="font-size: 20px;"> Data Generators: Augmented text and image data (batch size: 32).
+            - <p style="font-size: 20px;"> Compile: Adam optimizer, categorical cross-entropy loss, accuracy metric.
+            - <p style="font-size: 20px;"> Train/Validation Split: 80/20.
 
-            Results: Achieved performance with  87% on training set, 80% on validation set over 10 epochs.
+            <p style="font-size: 20px;"> Results: Achieved performance over 80% on training set, 80% on validation set over 10 epochs.
             """
             ,
             unsafe_allow_html=True
             )
-        img_performance_pry_NEU = Image.open("Images/5_DNN_Model/performance_pry_NEU.png")
+        img_performance_pry_NEU = Image.open("Images/5_DNN_Model/performance_pry_NEU.jpg")
         st.image(img_performance_pry_NEU)
 
     with st.container():
@@ -98,46 +94,46 @@ def app():
         
         st.write(
                 """
-            Overview:
-             - Combines: Text and image features for product classification.
-             - Text Preprocessing: Tokenization, stop word removal, synonym augmentation, TF-IDF vectorization (5,000 features).
-             - Image Processing: EfficientNetB0 for feature extraction, resizing, normalization, augmentation.
+            <p style="font-size: 20px;"> Overview:
+             - <p style="font-size: 20px;"> Combines: Text and image features for product classification.
+             - <p style="font-size: 20px;"> Text Preprocessing: Tokenization, stop word removal, synonym augmentation, TF-IDF vectorization (5,000 features).
+             - <p style="font-size: 20px;"> Image Processing: EfficientNetB0 for feature extraction, resizing, normalization, augmentation.
 
-            Text Branch:
-             - Layers: Dense (512 neurons, ReLU) → Batch Normalization → Dropout (25%) → Dense (256 neurons, ReLU) → Batch Normalization → Dropout (50%).
+            <p style="font-size: 20px;"> Text Branch:
+             - <p style="font-size: 20px;"> Layers: Dense (512 neurons, ReLU) → Batch Normalization → Dropout (25%) → Dense (256 neurons, ReLU) → Batch Normalization → Dropout (50%).
 
-            Image Branch:
-             - Layers: Dense (256 neurons, ReLU) → Batch Normalization → Dropout (50%) → Dense (128 neurons, ReLU) → Batch Normalization → Dropout (50%).
+            <p style="font-size: 20px;"> Image Branch:
+             - <p style="font-size: 20px;"> Layers: Dense (256 neurons, ReLU) → Batch Normalization → Dropout (50%) → Dense (128 neurons, ReLU) → Batch Normalization → Dropout (50%).
             """
             ,
             unsafe_allow_html=True
             )
 
-        img_Layers_Vitmer_Neu = Image.open("Images/5_DNN_Model/Layers_Vitmer_Neu.png")
+        img_Layers_Vitmer_Neu = Image.open("Images/5_DNN_Model/Layers_Vitmer_Neu.jpg")
         st.image(img_Layers_Vitmer_Neu)
 
         st.write(
                 """
-            Combined Branch:
-            - Layers: Concatenation → Dense (64 neurons, ReLU) → Batch Normalization → Dropout (50%) → Output (softmax activation).
+            <p style="font-size: 20px;"> Combined Branch:
+            - <p style="font-size: 20px;"> Layers: Concatenation → Dense (64 neurons, ReLU) → Batch Normalization → Dropout (50%) → Output (softmax activation).
 
-            Training:
-             - Compile: Nadam optimizer, sparse categorical cross-entropy loss.
-             - Callbacks: Early stopping, learning rate reduction.
-             - Performance: Achieves over 90% accuracy on both training and test sets.
+            <p style="font-size: 20px;"> Training:
+             - <p style="font-size: 20px;"> Compile: Nadam optimizer, sparse categorical cross-entropy loss.
+             - <p style="font-size: 20px;"> Callbacks: Early stopping, learning rate reduction.
+             - <p style="font-size: 20px;"> Performance: Achieves over 85% accuracy on both training and test sets.
             
-            Key Metrics:
-             - Detailed classification report and confusion matrix: Shows high precision across all product types.
+            <p style="font-size: 20px;"> Key Metrics:
+             - <p style="font-size: 20px;"> Detailed classification report and confusion matrix: Shows high precision across all product types.
             """
             ,
             unsafe_allow_html=True
             )
-        img_Vitaly_Acc_Loss = Image.open("Images/5_DNN_Model/Vitaly_Acc_Loss.png")
+        img_Vitaly_Acc_Loss = Image.open("Images/5_DNN_Model/Vitaly_Acc_Loss.jpg")
         st.image(img_Vitaly_Acc_Loss)
 
-        img_Vitaly_Classification_report = Image.open("Images/5_DNN_Model/Vitaly_Classification_report.png")
+        img_Vitaly_Classification_report = Image.open("Images/5_DNN_Model/Vitaly_Classification_report.jpg")
         st.image(img_Vitaly_Classification_report)
 
-        img_Vitaly_CM = Image.open("Images/5_DNN_Model/Vitaly_CM.png")
+        img_Vitaly_CM = Image.open("Images/5_DNN_Model/Vitaly_CM.jpg")
         st.image(img_Vitaly_CM)
     
